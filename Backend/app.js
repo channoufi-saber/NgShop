2210403ffa997dc2c0c967c185fbca79a9478804
channoufi-saber <saber.channoufi@esprit.tn>
2025-01-26 16:59:16 +0100

    Backend  Users & Authentication

--- Backend/app.js
+++ Backend/app.js
@@ -3,9 +3,12 @@ const app=express();
 const morgan=require('morgan')
 const mongoose=require('mongoose');
 const { Product } = require('./models/product');
-const productsRouter=require('./routers/products')
-const categoriesRouter=require('./routers/categories')
-const cors=require('cors')
+const productsRoutes=require('./routes/products')
+const categoriesRoutes=require('./routes/categories')
+const usersRoutes = require('./routes/users');
+const cors=require('cors');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 require('dotenv/config')
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -13,10 +16,12 @@ const api=process.env.API_URL
 app.use(cors())
 app.options('*',cors())
 
+app.use(authJwt())
+app.use(errorHandler)
 
-
-app.use(`${api}/products`,productsRouter);
-app.use(`${api}/categories`,categoriesRouter);
+app.use(`${api}/products`,productsRoutes);
+app.use(`${api}/categories`,categoriesRoutes);
+app.use(`${api}/users`, usersRoutes);
 
 mongoose.connect('mongodb://127.0.0.1:27017/eshop-database').then(()=>{
     console.log('database connected...')
